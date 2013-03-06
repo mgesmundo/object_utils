789abee4bb4f0657c0a19b794171b3bc3b8d2b0d
Marcello Gesmundo <mgesmundo@yoovant.com>
2013-03-06 19:00:29 +0100

    removed unwanted jsduck option

--- gen_doc.sh
+++ gen_doc.sh
@@ -7,5 +7,4 @@ jsduck  object_utils.js \
         --output="doc" \
         --title="object_utils documentation" \
 		--footer="Copyright (c) 2012-2013 Yoovant by Marcello Gesmundo" \
-        --warnings=-link,-dup_member,-no_doc \
-		--touch-examples-ui
+        --warnings=-link,-dup_member,-no_doc
